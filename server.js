c97ea0e18dbd92fbfdae6d5600f04c167ee545c2
mahirTheCoder <mahirchowdhury518@gmail.com>
2026-06-29 01:06:46 +0600

    extructure

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('server is running ');
+const express = require ('express')
+const router = require ('./routes')
+require('dotenv').config()
+const PORT = process.env.PORT
+
+const app = express()
+app.use(express.json())
+app.use(router)
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
