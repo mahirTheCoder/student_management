30d39fc4911f445aa5eb55ff019cfaaf738965f4
mahirTheCoder <mahirchowdhury518@gmail.com>
2026-07-20 23:03:00 +0600

    find all user

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,4 +1,16 @@
-const adminCheck = (req, res, next) => {
+
+const userSchema = require("../models/userSchema");
+
+
+const adminCheck = async  (req, res, next) => {
+
+  const allUsers = await userSchema.find({ role: [ 'student', 'teacher' ] });
+
+  if (!allUsers) {
+    return res.status(403).json({ message: "Access denied. Admins only." });
+  }
+  console.log("All Users:", allUsers);
+
 res.status(200).json({ message: "Admin check successful" });
 };
 
