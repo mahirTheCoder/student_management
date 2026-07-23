4ccde8c0b5e89065ee4e77105bb222abe5f4eef7
mahirTheCoder <mahirchowdhury518@gmail.com>
2026-07-23 23:34:58 +0600

    student usere route add

--- routes/dashboard_route/admin.js
+++ routes/dashboard_route/admin.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { allUserCheck , approvedUserCheck , deleteUserCheck} = require("../../controllers/adminController");
+const { allUserCheck , approvedUserCheck , deleteUserCheck , studentUserCheck} = require("../../controllers/adminController");
 
 
 
 router.get("/allUserCheck", allUserCheck);
+router.get("/studentUserCheck", studentUserCheck);
 router.patch("/approvedUserCheck/:id", approvedUserCheck);
-router.patch("/approvedUserCheck/:id", approvedUserCheck);
+router.delete("/deleteUserCheck/:id", deleteUserCheck);
 router.delete("/deleteUserCheck/:id", deleteUserCheck);
 
 module.exports = router;
